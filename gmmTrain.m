af924aa96973be781b83360326d71e9cec0175a6
kyle <kiskile@gmail.com>
2016-03-25 17:46:13 -0400

    simply analysis for mdcc

--- gmmTrain.m
+++ gmmTrain.m
@@ -19,19 +19,23 @@ function gmms = gmmTrain( dir_train, max_iter, epsilon, M )
 
 DD = dir(dir_train);
 gmms ={};
-%pesudo code
 % Initialize theta
-i = 0;
-prev_L = -Inf;
-improvement = Inf;
-while i =< max_iter and improvement >= epsilon
-    L = ComputeLikelihood(X,theta);
-    theta = UpdateParameters(theta,X,L);
-    improvement +L- prev_L
-    prev_L = L
-    i = i+1
+%i = 0;
+%prev_L = -Inf;
+%improvement = Inf;
+%while i =< max_iter and improvement >= epsilon
+%   L = ComputeLikelihood(X,theta);
+%    theta = UpdateParameters(theta,X,L);
+%    improvement +L- prev_L
+%    prev_L = L
+%    i = i+1
     
-end
+%end
 
-
-function likelihood = computeLL()
+% analysis for input mfcc
+% In each Training sub folders(under Training), there is a list of files 
+% including(x.mfccx.txt, x.wav) where x is the name of contents.
+% Each sub folder name is the name of speaker.
+% Then, all Training sub folders are similar to each other.
+% (speech contents) but different speakers.
+    
