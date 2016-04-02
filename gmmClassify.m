38feecd2bbead142fdabe05e3606f5d5073c6e82
Tarang <tarang.marathe@gmail.com>
2016-04-02 17:35:09 -0400

    initial commit

--- gmmClassify.m
+++ gmmClassify.m
@@ -1,3 +1,12 @@
+function result = gmmClassify( testdir, theta )
 % gmmClassify is a scripts that calculates 
 % and reports the likehoods of the five 
 % most likely speakers for each test utterance.
+DD = dir(testdir);
+
+%to get each data file load
+test_file = load(strcat(dir_test,'/',files(1).name));
+
+
+result = 1;
+end
