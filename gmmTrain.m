cd474ffa21c53bd82749c502802b89a91db17eca
kyle <kiskile@gmail.com>
2016-04-03 07:37:14 -0400

    debugged

--- gmmTrain.m
+++ gmmTrain.m
@@ -28,48 +28,43 @@ gmm = struct();
 % input dir, for Training data with all speakers' foler
 DD = dir(dir_train);
 
-
-
-% a.initializing theta
-% a-1. Initialize omega_m randomly with constrains.
-omega = ones(1,M)*(1/M);
-% a-2. Initialize each mu_m to a random vector from the data
-mu = zeros(8,14)
-msize = numel(mfcc_matrix);
-for m=1:8
-    row=mfcc_matrix(randperm(msize,14))
-    mu(m,:)=mu(m,:)+row
-end
-% a-3. Initialize sigma_m to a identity matrix.d =14.
-for n=1:M
-    sigma(:,:,n) =eye(d);
-end
-    
-    
-
-% theta ={w,u,E}
-theta = {omega,mu,sigma};
-prev_L = -Inf;
-improvement = Inf;  
-
 for i=4:length(DD)
     % for each speaker, the directory of their mfcc files.
     speaker_dir = strcat(dir_train,'/',DD(i).name);
     mfcc_dir = dir( [ strcat(dir_train,'/',DD(i).name), filesep, '*', 'mfcc'] );
+    
     % fact: there are always 9 mfcc files for each speaker.
     for w=1:length(mfcc_DD)
         % mfcc input is mfcc_dir(i), set up matrix for it.
         matrix = [];
         mfcc_matrix = [matrix;dlmread([speaker_dir, filesep, mfcc_dir(1).name])];
-     
-        
-        
+    end
+    D = size(mfcc_matrix,2);
+    % a.initializing theta
+    % a-1. Initialize omega_m randomly with constrains.
+    omega = ones(1,M)*(1/M);
+    % a-2. Initialize each mu_m to a random vector from the data
+    mu = zeros(M,D);
+    msize = numel(mfcc_matrix);
+    for m=1:M   
+        row = mfcc_matrix(randperm(msize,D));
+        mu(m,:) = mu(m,:)+row;
+    end
+    % a-3. Initialize sigma_m to a identity matrix.
+    for n=1:M
+        sigma(:,:,n) =eye(D);
     end
 end
+% theta ={w,u,E}
+theta = {omega,mu,sigma};
+prev_L = -Inf;
+improvement = Inf;  
+
 i=0;
-while i<= max_iter & improvement >= epsilon
-    L:= ComputeLikelihood(X,theta);
-    theta:= UpdateParameters(theta,X,L);
+D = size(mfcc_matrix,2);
+while i<= max_iter && improvement >= epsilon
+    L = ComputeLikelihood(X,theta);
+    theta = UpdateParameters(theta,X,L);
     
 end
     
@@ -82,7 +77,7 @@ end
 
 % helper functions as list below:
 
-function L = ComputeLikelihood(max_iter, epsilon, M)
+function L = ComputeLikelihood(D,T,max_iter, epsilon, M)
 % ComputeLikelihood
 % inputs:
 %           max_iter   : maximum number of training iterations (integer)
@@ -92,8 +87,14 @@ function L = ComputeLikelihood(max_iter, epsilon, M)
 for m=1:M
     wmbm= zeros(M,9)
 end
+% find bm, where x, mu,cov are d dimension vectors.
+b = zeros(T,M)
+denom = (2*pi)^(d/2)*sqrt(prod(sigma))
+numer = 
 
 
+
+end
 
 function theta = UpdateParam(max_iter, epsilon, M)
 % UpdateParam
