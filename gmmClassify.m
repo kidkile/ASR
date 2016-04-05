1efcc060fd2b739c93e1ee1b1ab3434436b1c126
kyle <kiskile@gmail.com>
2016-04-05 14:43:25 -0400

    update to save

--- gmmClassify.m
+++ gmmClassify.m
@@ -1,12 +1,15 @@
-function result = gmmClassify( testdir, theta )
-% gmmClassify is a scripts that calculates 
-% and reports the likehoods of the five 
-% most likely speakers for each test utterance.
-DD = dir(testdir);
-
-%to get each data file load
-test_file = load(strcat(dir_test,'/',files(1).name));
-
-
-result = 1;
+function classification = gmmClassify(dir_test,likelihoods,M)
+% dir_train = '/Users/menglongji/Desktop/ASR/speechdata/Training'
+% dir_test = '/Users/menglongji/Desktop/ASR/speechdata/Testing'
+% max_iter = 10;
+% epsilon = 0.1;
+% M = 8;
+% 
+% mfcc_dir = dir( [ dir_test, filesep, '*', 'mfcc'] );
+gmms = gmmTrain( dir_train, max_iter, epsilon, M );
+for i=1:length(mfcc_dir)
+    mfcc = dlmread([dir_test,filesep,mfcc_dir(i).name]);
+    for w=1:length(gmms)
+        gmm = gmms(w);
+    end
 end
