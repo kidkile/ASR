3cb4c6e9cb61544cb11bfa0bd609bf95ee12dbf2
Tarang <tarang.marathe@gmail.com>
2016-04-05 15:18:53 -0400

    more chnges

--- myTrain.m
+++ myTrain.m
@@ -6,3 +6,7 @@ Q = 3;
 initType = 'kmeans';
 max_iter = 3;
 output_file = './hmm/';
+
+speakers = dir([train_dir, filesep])
+
+TIMITlst = {'b','d','g','p','t','k','dx','q','jh','ch','s','sh','z','zh','f','th','v', 'dh','m','n','ng','em','en','eng','nx','l','r','w','y','hh','hv','el','iy','ih', 'eh','ey','ae','aa','aw','ah','ao','oy','ow','uh','uw','ux','er','ax','ix','axr', 'ax-h','pau','epi','h#','1','2','bcl','dcl', 'gcl', 'pcl', 'tcl', 'kcl'};
