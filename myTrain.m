f4c51c0430198ed2c600da53bc5629ea681d43b3
Tarang <tarang.marathe@gmail.com>
2016-04-06 01:08:47 -0400

    fixed mfcc loading issue

--- myTrain.m
+++ myTrain.m
@@ -1,12 +1,31 @@
-train_dir = 'speechdata/Training'
 % Remember to change for CDF file structure
-bnt_dir = 'bnt'
+dir_train = 'speechdata/Training';
+bnt_dir = 'bnt';
+
+%addpath(genpath(bnt_dir));
+
 M = 8;
 Q = 3;
 initType = 'kmeans';
 max_iter = 3;
 output_file = './hmm/';
 
-speakers = dir([train_dir, filesep])
+HMM = struct();
+
+speakers = dir([train_dir, filesep]);
 
 TIMITlst = {'b','d','g','p','t','k','dx','q','jh','ch','s','sh','z','zh','f','th','v', 'dh','m','n','ng','em','en','eng','nx','l','r','w','y','hh','hv','el','iy','ih', 'eh','ey','ae','aa','aw','ah','ao','oy','ow','uh','uw','ux','er','ax','ix','axr', 'ax-h','pau','epi','h#','1','2','bcl','dcl', 'gcl', 'pcl', 'tcl', 'kcl'};
+
+for i = 1:length(speakers)
+    speaker_name = speakers(i).name;
+    if not(strcmp(speaker_name, '.') || strcmp(speaker_name, '..'))
+        speaker_path = [dir_train, filesep, speaker_name];
+        mfccs = dir([speaker_path, filesep, '*.mfcc']);
+        phns = dir([speaker_path, filesep, '*.phn']);
+        
+        for j = 1:length(mfccs)
+            mfcc_data = load([speaker_path, filesep, mfccs(j).name]);
+        end
+        
+    end 
+end
