d4b94895a1cd2ba9c36c95b5f5866a1d73c2a735
Tarang <tarang.marathe@gmail.com>
2016-04-08 04:09:35 -0400

    fixed stupid bugs in dimensionality

--- myRun.m
+++ myRun.m
@@ -1,45 +1,70 @@
 dir_test = 'speechdata/Testing';
-hmm = load('hmm/standard_HMM.mat');
-hmm = hmm.trainedHMM;
+
 bnt_dir = '/bnt/';
 dimensions = 14;
 
-correct = 0;
-total = 0;
-addpath(genpath(bnt_dir));
+results = struct();
 
-phn_files = dir([dir_test, filesep, '*.phn']);
-mfcc_files = dir([dir_test, filesep, '*.mfcc']);
-for i = 1:length(phn_files)
-    mfcc = load([dir_test, filesep, mfcc_files(i).name]);
-    phn = textread([dir_test, filesep, phn_files(i).name], '%s', 'delimiter', '\n');
-    
-    total = total + length(phn);
-    
-    for j = 1:length(phn)
-        phn_data = strsplit(' ', phn{j});
-        phn_start = (str2num(phn_data{1}) / 128) + 1;
-        phn_end = min((str2num(phn_data{2}) / 128) + 1, size(mfcc, 1));
-        if strcmp(phn_data{3}, 'h#')
-            phoneme = 'sil';
-        else
-            phoneme =  phn_data{3};
-        end
-        mfcc_phoneme = transpose(mfcc(phn_start:phn_end, 1:dimensions));
+all_hmms = dir (['hmm', filesep, '*.mat']);
+for l = 1:length(all_hmms)
+    if not(strcmp(all_hmms(l).name, 'standard2_HMM.mat') || strcmp(all_hmms(l).name, 'standard_HMM.mat'))
         
-        max_log_prob = -Inf;
-        phn_prediction = '';
-        unique_phns = fieldnames(hmm);
-        for k = 1:length(unique_phns)
-            log_prob = loglikHMM(hmm.(unique_phns{k}), mfcc_phoneme);
-            if log_prob > max_log_prob
-                max_log_prob = log_prob;
-                phn_prediction = unique_phns{k};
-            end    
+        hmm_file = all_hmms(l).name;
+        hmm = load(['hmm', filesep, hmm_file]);
+        hmm = hmm.trainedHMM;
+        
+        if strcmp(hmm_file, 'dim10_iter20_HMM.mat')
+            dimensions = 10;
+        elseif strcmp(hmm_file, 'dim1_iter20_HMM.mat')
+            dimensions = 1;
+        elseif strcmp(hmm_file, 'dim3_iter20_HMM.mat')
+            dimensions = 3;
+        elseif strcmp(hmm_file, 'dim7_iter20_HMM.mat')
+            dimensions = 7;
         end
-        if strcmp(phoneme, phn_prediction)
-            correct = correct + 1;
+        
+        correct = 0;
+        total = 0;
+        addpath(genpath(bnt_dir));
+
+        phn_files = dir([dir_test, filesep, '*.phn']);
+        mfcc_files = dir([dir_test, filesep, '*.mfcc']);
+        for i = 1:length(phn_files)
+            mfcc = load([dir_test, filesep, mfcc_files(i).name]);
+            phn = textread([dir_test, filesep, phn_files(i).name], '%s', 'delimiter', '\n');
+
+            total = total + length(phn);
+
+            for j = 1:length(phn)
+                phn_data = strsplit(' ', phn{j});
+                phn_start = (str2num(phn_data{1}) / 128) + 1;
+                phn_end = min((str2num(phn_data{2}) / 128) + 1, size(mfcc, 1));
+                if strcmp(phn_data{3}, 'h#')
+                    phoneme = 'sil';
+                else
+                    phoneme =  phn_data{3};
+                end
+                mfcc_phoneme = transpose(mfcc(phn_start:phn_end, 1:dimensions));
+
+                max_log_prob = -Inf;
+                phn_prediction = '';
+                unique_phns = fieldnames(hmm);
+                for k = 1:length(unique_phns)
+                    log_prob = loglikHMM(hmm.(unique_phns{k}), mfcc_phoneme);
+                    if log_prob > max_log_prob
+                        max_log_prob = log_prob;
+                        phn_prediction = unique_phns{k};
+                    end    
+                end
+                if strcmp(phoneme, phn_prediction)
+                    correct = correct + 1;
+                end
+            end
+
         end
+        name = strsplit('.', hmm_file);
+        results.(name{1}) = correct;
+        disp(correct);
+        disp(total);
     end
-    
 end
