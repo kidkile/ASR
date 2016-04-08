6feb39af90bb2d277b49790bd125da89ad48c291
Tarang <tarang.marathe@gmail.com>
2016-04-08 18:31:36 -0400

    part 4 almost done

--- Levenshtein.m
+++ Levenshtein.m
@@ -10,25 +10,63 @@ function [SE IE DE LEV_DIST] =Levenshtein(hypothesis,annotation_dir)
 %	LEV_DIST: proportion of overall error in all hypotheses
 
 hyp = textread(hypothesis, '%s', 'delimiter', '\n');
-ref_count = 0;
+ref_word_count = 0;
 SE = 0;
 IE = 0;
 DE = 0;
 for i = 1:length(hyp)
    ref = textread([annotation_dir, filesep, 'unkn_', num2str(i), '.txt'], ...
        '%s', 'delimiter', '\n');
-   hyp_parts = strsplit(' ', hyp{i});
-   hyp_sentence = hyp_parts{3};
+   hyp_parts = strsplit(hyp{i}, ' ');
+   hyp_sentence = hyp_parts(3:end);
    
-   ref_parts = strsplit(' ', ref{1});
+   ref_parts = strsplit(ref{1}, ' ');
    ref_sentence = ref_parts(3:end);
-   ref_count = ref_count + length(ref_sentence);
-    
+   ref_length = length(ref_parts);
+   ref_word_count = ref_word_count + ref_length; % minus 2 to account for leading bit values
+   
+   n = length(ref_sentence);
+   m = length(hyp_sentence);
+   dist_mat = zeroes(n+1, m+1);
+   dist_mat(1, :) = Inf;
+   dist_mat(:, 1) = Inf;
+   dist_mat(1, 1) = 0;
+   back_mat = zeroes(n+1, m+1);
+   for i = 1:n
+       for j = 1:m
+           del = dist_mat(i, j+1) + 1;
+           ins = dist_mat(i+1, j) + 1;
+           miss = dist_mat(i, j) +1;
+           hit = dist_mat(i, j) + not(strcmp(ref_sentence{i}, hyp_sentence{j}));
+           
+           [dist_mat(i+1, j+1), am] = min([del, ins, miss, hit], [], 2);
+           back_mat(i+1, j+1) = am;
+       end
+   end
+   error_counter = zeroes(1, 4);
+   i = n+1;
+   j = m+1;
+   while not((j == 1) && (i == 1))
+       am = back_mat(i, j);
+       error_counter(am) = error_counter(am) + 1;
+       i = i - (am ~= 2);
+       j = j - (am ~= 1);
+   end
+   
+   disp(SE / ref_length)
+   disp(SE / ref_length)
+   disp(SE / ref_length)
+   disp((SE + IE + DE) / ref_length)
+   
+   SE = SE + error_counter(1);
+   IE = IE + error_counter(2);
+   DE = DE + error_counter(3);
+   
 end
 
-SE = SE / ref_count;
-IE = IE / ref_count;
-DE = DE / ref_count;
+SE = SE / ref_word_count;
+IE = IE / ref_word_count;
+DE = DE / ref_word_count;
 LEV_DIST = SE + IE + DE;
 
 return
--- ibmSpeech.m
+++ ibmSpeech.m
@@ -1,13 +1,20 @@
 
 
-speech_txt_dir = '/Users/menglongji/desktop/ASR/speech.txt'
-f = fopen(sppech_txt_dir,w);
+speech_txt_dir = 'speech.txt'
+f = fopen(speech_txt_dir, 'w');
 for i=1:30
-    flac_dir = strcat('/u/cs401/speechdata/Testing/unkn_',num2str(i),'.flac');
+    % fix directories for cdf
+    flac_dir = ['speechdata/Testing/unkn_', num2str(i), '.flac'];
+    
     username='5e0c57e7-e358-43bd-a01e-a41ddd2c612a';
     password='8ygvoOxxLVTT';
-    c1='--header "Content-Type: audio/flac"';
-    c2='--header "Transfers-Encoding: chunked"';
-    c3= '--data-binary @%s';
-    http='https://stream.watsonplatform.net/speech-to-text/api';
+    c1=' --header "Content-Type: audio/flac"';
+    c2=' --header "Transfers-Encoding: chunked"';
+    c3= [' --data-binary @', flac_dir];
+    http=' "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?continuous=true"';
+    curl = ['curl -u ', username, ':', password, ' -X POST', ...
+        c1, c2, c3, http];
+    [status, result] = unix(curl);
+    data = JSON.parse(result);
+    disp(data.results{1, 1}.alternatives{1, 1}.transcript);
 end
