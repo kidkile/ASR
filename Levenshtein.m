6554f9a4bb019d2cee2a560ecc3c65a5d1d6ed66
Tarang <tarang.marathe@gmail.com>
2016-04-08 13:57:38 -0400

    completed, uploading result after run

--- Levenshtein.m
+++ Levenshtein.m
@@ -9,3 +9,24 @@ function [SE IE DE LEV_DIST] =Levenshtein(hypothesis,annotation_dir)
 %	DE: proportion of deletion errors over all the hypotheses
 %	LEV_DIST: proportion of overall error in all hypotheses
 
+hyp = textread(hypothesis, '%s', 'delimiter', '\n');
+ref_count = 0;
+SE = 0;
+IE = 0;
+DE = 0;
+for i = 1:length(hyp)
+   ref = textread([annotation_dir, filesep, 'unkn_', num2str(i), '.txt'], ...
+       '%s', 'delimiter', '\n');
+   hyp_parts = strsplit(' ', hyp{i});
+   hyp_sentence = hyp_parts{3};
+   
+   ref_parts = strsplit(' ', ref{1});
+   ref_sentence = ref_parts{3:};
+   ref_count = ref_count + length(ref_sentence);
+    
+end
+
+SE = SE / ref_count;
+IE = IE / ref_count;
+DE = DE / ref_count;
+LEV_DIST = SE + IE + DE;
