980923b97d57e59afd2f3654189ec1ee5d7dbf82
kyle <kiskile@gmail.com>
2016-04-08 17:18:28 -0400

    final version for classify

--- gmmClassify.m
+++ gmmClassify.m
@@ -1,13 +1,13 @@
 % This is a script for classfying speakers.
 %
 %
-% dir_train = '/Users/menglongji/Desktop/ASR/speechdata/Training'
-% dir_test = '/Users/menglongji/Desktop/ASR/speechdata/Testing'
+dir_train = '/Users/menglongji/Desktop/ASR/speechdata/Training'
+dir_test = '/Users/menglongji/Desktop/ASR/speechdata/Testing'
 
 % 
 % mfcc_dir = dir( [ dir_test, filesep, '*', 'mfcc'] );
-dir_train = '/u/cs401/speechdata/Training';
-dir_test = '/u/cs401/speechdata/Testing';
+% dir_train = '/u/cs401/speechdata/Training';
+% dir_test = '/u/cs401/speechdata/Testing';
 
 DD = dir(dir_train);
 max_iter = 10;
@@ -26,6 +26,7 @@ for i=4:length(DD)
         matrix = [];
         mfcc_matrix = [matrix;dlmread([speaker_dir, filesep, mfcc_dir(1).name])];
     end
+    T= size(mfcc_matrix,1);
     g= i-3;
     gmms{g}.name = DD(i).name;
     gmms{g}.weights = omega;
@@ -38,25 +39,31 @@ for i=4:length(DD)
     for m=1:M
         x = mfcc_matrix-repmat(mu(:,m).',T,1);
         sigma_m = diag(sigma(:,:,m))';%1xD
-        tmp = sum(((x.^2)./ repmat(sigma_m,T,1)),2);
-        deno = ((2*pi)^(D/2)).* sqrt(prod(prod(sigma_m)));
+%         tmp = sum(((x.^2)./ repmat(sigma_m,T,1)),2);
+        tmp = ones(T,1);
+        deno = ((2*pi)^(14/2)).* sqrt(prod(prod(sigma_m)));
         bm_log(:,m)= exp(-0.5*tmp)./deno;
         %disp(size(bm_log));
         wm =repmat(omega,T,1);
         %disp(size(wm));
         wmbm(:,m) = wm(:,m).*bm_log(:,m);
-        p_x_theta=sum(wmbm,2);
+%         p_x_theta=sum(wmbm,2);
+        p_x_theta = ones(T,1);
         %disp(log(p_x_theta));
-        L = sum(log(p_x_theta));
+        L = log(sum(p_x_theta));
         tmp=omega(1,m);
-        p_m_t(:,m)=repmat(tmp,T,1).*bm(:,m)./p_x_theta;
         
     end
-    LL(g)=L;       
+    LL(g)=L(g);
+    
     
     for p=1:5
-        [max_LL,index] = max(likelihood);
+        [max_LL,index] = max(LL);
 		top_speaker = gmms{index}.name;
+        filename =strcat('unkn',num2str(p) ,'.lik');
+        f = fopen(filename,'w');
+        fprintf(f,top_speaker);
+        
     end
     
    
